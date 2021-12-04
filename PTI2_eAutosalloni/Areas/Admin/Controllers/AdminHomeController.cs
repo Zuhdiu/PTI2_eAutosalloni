9c00940409d37c53c70cbecd1dbdccdf810ed8b0
Zuhdi Krasniqi <zuhdi.krasniqi@hotmail.com>
2021-12-04 23:15:05 +0100

    Add project files.

---
+++ PTI2_eAutosalloni/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTI2_eAutosalloni.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        [Area("Admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PTI2_eAutosalloni/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PTI2_eAutosalloni.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTI2_eAutosalloni.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
